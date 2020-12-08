940be9efff51c6e57d7389d9dc630e456f6ce4c0
Micah Banschick <micha.ban@gmail.com>
2020-12-08 22:12:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  # get 'artists/show'
-  #
-  # get 'artists/index'
-  #
-  # get 'songs/show'
-  #
-  # get 'songs/index'
-  #
-  # get 'song/show'
-  #
-  # get 'song/index'
 
   resources :artists, :songs
 end
